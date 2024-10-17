dbae797a50696302661b58e55ffb93afbeb8906c
Weyers <h.m.weyers@student.tue.nl>
2024-10-17 11:06:01 +0200

    added more intersection methods to collidable

--- src/engine/math/Collideable.java
+++ src/engine/math/Collideable.java
@@ -13,4 +13,18 @@ public interface Collideable {
     static boolean collides(Vector2D point, Collideable collideable){
         return collideable.getBoundingBox().contains(point);
     }
+
+    /**
+     * @return if a collideable's bounding box contains a points
+     */
+    static boolean collides(Collideable collideable, Vector2D point){
+        return collides(point, collideable);
+    }
+
+    /**
+     * @return if two collideable's bounding boxes have overlap
+     */
+    static boolean collides(Collideable obj1, Collideable obj2){
+        return obj1.getBoundingBox().intersects(obj2.getBoundingBox());
+    }
 }
